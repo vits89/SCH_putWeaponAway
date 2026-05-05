2c36d5657f1272505fc9b83e3a72659d91f10065
Schatten <sv89@tut.by>
2026-05-05 23:18:46 +0400

    Add missed class

--- config.cpp
+++ config.cpp
@@ -1,12 +1,15 @@
 class CfgPatches
 {
-	name = "Put Weapon Away";
-	author = "Schatten";
-	url = "https://github.com/vits89";
-	requiredVersion = 2.06;
-	requiredAddons[] = { "A3_Language_F" };
-	units[] = { };
-	weapons[] = { };
+	class SCH_putWeaponAway
+	{
+		name = "Put Weapon Away";
+		author = "Schatten";
+		url = "https://github.com/vits89";
+		requiredVersion = 2.06;
+		requiredAddons[] = { "A3_Language_F" };
+		units[] = { };
+		weapons[] = { };
+	};
 };
 
 class CfgFunctions
