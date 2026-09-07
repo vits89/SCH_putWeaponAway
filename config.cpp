9f3624bfbf2cd6e8cf0268420e4c5ca71c348889
Schatten <vitsor89@gmail.com>
2026-09-07 21:23:38 +0400

    Remove animation fixes

--- config.cpp
+++ config.cpp
@@ -6,11 +6,7 @@ class CfgPatches
 		author = "Schatten";
 		url = "https://github.com/vits89";
 		requiredVersion = 2.06;
-		requiredAddons[] =
-		{
-			"A3_Anims_F_Config_Sdr",
-			"A3_Language_F"
-		};
+		requiredAddons[] = { "A3_Language_F" };
 		units[] = { };
 		weapons[] = { };
 	};
@@ -27,4 +23,3 @@ class CfgHints
 };
 
 #include "action.cpp"
-#include "moves.cpp"
